cadad1b70dcb9f93b6d24dc26950e02f66b5d74a
Fumiaki Takizawa <tkzwfm21@gmail.com>
2018-05-17 00:35:03 +0900

    count_characters.py modify

--- count_characters.py
+++ count_characters.py
@@ -1,3 +1,12 @@
+from collections import defaultdict
+from collections import Counter
+
+def count_characters2(string2):
+    count_dict2 = defaultdict(int)
+    for c in string2:
+        count_dict2[c] += 1
+    print(count_dict2)
+
 def count_characters(string):
     count_dict = {}
     for c in string:
@@ -9,4 +18,7 @@ def count_characters(string):
 
 count_characters('Dynasty')
 count_characters('12345anagram')
+count_characters2('Dynasty')
+count_characters2('12345anagram')
+print(Counter('Dynasty'))
 input()
